00a0ff48876669124163b3e5bd66a731c374bcbf
Samir Khanal <gdssamir@gmail.com>
2025-01-25 13:20:55 +0545

    email send setup

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
+import ejs from "ejs";
+import { sendMail } from "./config/mailer.js";
 
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// to get current js directory
 import path from "path";
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url)); // gets directory of current js module
 
-// finding from the found directory
 // set view engine --render EJS templates from a "views" folder in your project directory
 app.set("view engine", "ejs"); // use ejs
-app.set("views", path.resolve(__dirname, "./views")); // get views from views directory
+app.set("views", path.resolve(__dirname, "./views"));
 
-app.get("/", (req: Request, res: Response) => {
-  res.render("welcome"); // render welcome file
+app.get("/", async (req: Request, res: Response) => {
+  const html = await ejs.renderFile(__dirname + `/views/emails/welcome.ejs`, {
+    name: "Samir Khanal",
+  });
+  await sendMail("khanalsamir52@gmail.com", "Testing", html);
+  res.json({ msg: "email send successfully" });
+  // res.render("emails/welcome", { name: "Samir Khanal" }); // render welcome file
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
