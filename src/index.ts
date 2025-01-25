14ca8d95a1c640c4bc5e7e3edd2cdea98e6ad30d
Samir Khanal <gdssamir@gmail.com>
2025-01-25 09:36:50 +0545

    ejs setup

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,21 @@ import "dotenv/config";
 
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// to get current js directory
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // gets directory of current js module
+
+// finding from the found directory
+// set view engine --render EJS templates from a "views" folder in your project directory
+app.set("view engine", "ejs"); // use ejs
+app.set("views", path.resolve(__dirname, "./views")); // get views from views directory
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hey its working");
+  res.render("welcome"); // render welcome file
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
